04e22575a413339a456868ce3e2a64a8ce4676cb
george he <he.george10@gmail.com>
2021-10-16 17:53:51 -0400

    made the setState for repeat bool async
    and it works

--- components/timer.jsx
+++ components/timer.jsx
@@ -3,48 +3,55 @@ import { Text, TouchableOpacity, View } from "react-native";
 import Timer from "react-compound-timer";
 import { styles } from "../StyleSheet";
 
-export default function CountDown(props) {
-  const [title, setTitle] = useState({ text: "title" });
-  const [time, setTime] = useState(1100);
-  const [reon, setReon] = useState(false);
-  const [repeat, setRepeat] = useState({
-    count: 0,
-    on: false,
-    style: styles.colorGray,
-  });
-  const [rightButton, setRightButton] = useState({
-    text: "Start",
-    style: styles.greenButt,
-  });
+class CountDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      title: "",
+      time: 1100,
+      repeatCount: 0,
+      repeatOn: false,
+      repeatStyle: styles.colorGray,
+      rightButt: "Start",
+      rightButtstyle: styles.greenButt,
+    };
+  }
 
-  const toggleRepeat = () => {
-    if (repeat.on) {
-      let re = { count: 11, on: false, style: styles.colorGray };
-      setRepeat(re);
-      setReon(false);
+  async handleChange(event) {
+    console.log(this.state.repeatOn);
+    await this.setState({ title: "event.target.value" });
+    console.log(this.state.repeatOn);
+  }
+
+  async toggleRepeat() {
+    console.log("yeeeeee", this.state.repeatOn);
+
+    if (this.state.repeatOn) {
+      await this.setState({ repeatOn: false, repeatStyle: styles.colorGray });
     } else {
-      setRepeat({ ...repeat, on: true, style: styles.colorYellow });
-      setReon(true);
+      await this.setState({ repeatOn: true, repeatStyle: styles.colorYellow });
     }
-    console.log("upda", repeat);
-    console.log(reon);
-  };
+    console.log("upda", this.state.repeatOn);
+    // this.handleChange(this);
+    // console.log("upda", this.state.repeatOn);
+  }
 
-  const updateRepeat = () => {
+  updateRepeat() {
     console.log("update re", reon);
     if (repeat.on) {
       console.log("reeeeeeeee");
       setRepeat({ ...repeat, count: repeat.count + 1 });
     }
-  };
+  }
 
-  const leftButtonHandler = (reset, stop) => {
+  leftButtonHandler(reset, stop) {
     stop();
     reset();
     setRightButton({ text: "Start", style: styles.greenButt });
-  };
+  }
 
-  const rightButtonHandler = (timerState, start, pause) => {
+  rightButtonHandler(timerState, start, pause) {
     switch (timerState) {
       case "INITED":
       case "PAUSED":
@@ -59,75 +66,86 @@ export default function CountDown(props) {
       default:
         break;
     }
-  };
+  }
+  render() {
+    return (
+      <Timer
+        initialTime={this.state.time}
+        lastUnit="m"
+        timeToUpdate={1}
+        direction="backward"
+        startImmediately={false}
+        formatValue={(value) => `${value < 10 ? `0${value}` : value}`}
+        checkpoints={[
+          {
+            time: 0,
+            callback: () => updateRepeat(),
+          },
+        ]}
+      >
+        {({ start, pause, stop, reset, getTimerState }) => (
+          <View style={[styles.timerBox]}>
+            {/* Fav, repeat */}
+            <View>
+              <TouchableOpacity
+                style={{ flexDirection: "row" }}
+                onPress={() => {
+                  this.toggleRepeat();
+                }}
+              >
+                <Text style={this.state.repeatStyle}>Re </Text>
+                <Text style={this.state.repeatStyle}>
+                  {this.state.repeatCount}
+                </Text>
+              </TouchableOpacity>
+            </View>
 
-  return (
-    <Timer
-      initialTime={time}
-      lastUnit="m"
-      timeToUpdate={1}
-      direction="backward"
-      startImmediately={false}
-      formatValue={(value) => `${value < 10 ? `0${value}` : value}`}
-      checkpoints={[
-        {
-          time: 0,
-          callback: () => updateRepeat(),
-        },
-      ]}
-    >
-      {({ start, pause, stop, reset, getTimerState }) => (
-        <View style={[styles.timerBox]}>
-          {/* Fav, repeat */}
-          <View>
-            <TouchableOpacity
-              style={{ flexDirection: "row" }}
-              onPress={() => {
-                toggleRepeat();
-              }}
-            >
-              <Text style={repeat.style}>Re </Text>
-              <Text style={repeat.style}>{repeat.count}</Text>
-            </TouchableOpacity>
-          </View>
+            {/* Timer title */}
+            <View style={styles.rowCenter}>
+              <Text style={styles.colorYellow}>{this.state.title}</Text>
+            </View>
 
-          {/* Timer title */}
-          <View style={styles.rowCenter}>
-            <Text style={styles.colorYellow}>{title.text}</Text>
-          </View>
+            {/* Time */}
+            <View style={styles.rowCenter}>
+              <Text style={[styles.timerText, styles.colorYellow]}>
+                <Timer.Minutes />
+                <Text style={styles.colorGray}>:</Text>
+                <Timer.Seconds />
+              </Text>
+            </View>
 
-          {/* Time */}
-          <View style={styles.rowCenter}>
-            <Text style={[styles.timerText, styles.colorYellow]}>
-              <Timer.Minutes />
-              <Text style={styles.colorGray}>:</Text>
-              <Timer.Seconds />
-            </Text>
+            {/* Control */}
+            <View style={styles.buttons}>
+              {/* left button */}
+              <TouchableOpacity
+                style={[styles.button, styles.buttonBordered]}
+                onPress={() => {
+                  leftButtonHandler(reset, stop);
+                }}
+              >
+                <Text style={styles.colorYellow}>Reset</Text>
+              </TouchableOpacity>
+              {/* right button */}
+              <TouchableOpacity
+                style={[
+                  styles.button,
+                  styles.buttonBordered,
+                  this.state.rightButtstyle,
+                ]}
+                onPress={() => {
+                  rightButtonHandler(getTimerState(), start, pause);
+                }}
+              >
+                <Text style={this.state.rightButtstyle}>
+                  {this.state.rightButt}
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-
-          {/* Control */}
-          <View style={styles.buttons}>
-            {/* left button */}
-            <TouchableOpacity
-              style={[styles.button, styles.buttonBordered]}
-              onPress={() => {
-                leftButtonHandler(reset, stop);
-              }}
-            >
-              <Text style={styles.colorYellow}>Reset</Text>
-            </TouchableOpacity>
-            {/* right button */}
-            <TouchableOpacity
-              style={[styles.button, styles.buttonBordered, rightButton.style]}
-              onPress={() => {
-                rightButtonHandler(getTimerState(), start, pause);
-              }}
-            >
-              <Text style={rightButton.style}>{rightButton.text}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      )}
-    </Timer>
-  );
+        )}
+      </Timer>
+    );
+  }
 }
+
+export default CountDown;
